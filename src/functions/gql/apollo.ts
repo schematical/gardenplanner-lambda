8e0377c805ea99d9213b720a65ead5ac0a535116
Matt <mlea@schematical.com>
2022-04-22 15:45:09 -0500

    Started on the client

--- src/functions/gql/apollo.ts
+++ src/functions/gql/apollo.ts
@@ -1,12 +1,14 @@
 import config from "config";
 import {connect} from "mongoose";
-import {buildSchema} from "type-graphql";
+import {buildSchema, emitSchemaDefinitionFile} from "type-graphql";
 import {UserResolver} from "./user/User.resolver";
 import {CropSpeciesResolver} from "./crop-species/CropSpecies.resolver";
 import {Container} from "typedi";
 import {GeoLocationResolver} from "./geo-location/GeoLocation.resolver";
+import path from "path";
 
 export const getApolloConfig = async () => {
+    const debug = process.env.NODE_ENV === 'dev' ? true : false;
     const url = config.get<string>('db.host');
     await connect(
         url,
@@ -15,14 +17,18 @@ export const getApolloConfig = async () => {
     const schema = await buildSchema({
         resolvers: [UserResolver, CropSpeciesResolver, GeoLocationResolver],
         container: Container,
+        emitSchemaFile: debug ? path.resolve(__dirname, "schema.gql") : false, // debug
     });
+    if (debug) {
+        await emitSchemaDefinitionFile('/home/user1a/WebstormProjects/gardenplanner-lambda/schema.gql', schema);
+    }
     const apolloConfig = {
         schema,
         introspection: true,
         playground: {
             endpoint: '/dev/graphql',
         },
-        debug: true,
+        debug: debug,
         // plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
     }
     return apolloConfig;
